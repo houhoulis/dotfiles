d319ccd043bc7acdfca8ce542c301ce53de37f6f
Chris Houhoulis <chris@chrishouhoulis.com>
2020-12-04 20:18:40 -0500

    "Fix" nvm.fish (thanks masukomi).

--- fish/functions/nvm.fish
+++ fish/functions/nvm.fish
@@ -1,3 +1,4 @@
 function nvm
+    echo "Obviously it's not the right language for you (or anyone). You should erase the JS from your system and pretend it never happened. 😉"
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
