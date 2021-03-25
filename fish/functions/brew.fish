3f9ff516dda46bd228e204883ae28dc4637358b3
Chris Houhoulis <chris@chrishouhoulis.com>
2021-03-24 22:47:33 -0400

    Fix env var tests in brew.fish. Nicer output.

--- fish/functions/brew.fish
+++ fish/functions/brew.fish
@@ -4,15 +4,13 @@ function brew
     # For HOMEBREW_NO_INSTALL_CLEANUP, see https://brew.sh/2019/02/02/homebrew-2.0.0/
     # For HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS, see https://brew.sh/2020/12/21/homebrew-2.7.0/
 
-    # echo $HOMEBREW_NO_INSTALL_CLEANUP | read -l noinstall
-    # echo $HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS | read -l noperiodic
     set -l noinstall $HOMEBREW_NO_INSTALL_CLEANUP
     set -l noperiodic $HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS
 
-    if test -n $noinstall
-    and test -n $noperiodic
+    if test -n "$noinstall"
+    and test -n "$noperiodic"
     and test $noperiodic -ge 36500
-        echo -e "HOMEBREW_NO_INSTALL_CLEANUP is set to '$noinstall', and HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS is\n set to '$noperiodic', which seems sufficient."
+        echo -e "HOMEBREW_NO_INSTALL_CLEANUP is set to '$noinstall', and\nHOMEBREW_CLEANUP_PERIODIC_FULL_DAYS is set to '$noperiodic', which seems sufficient."
         read -l -P "Continue? [y/N] " confirm
         if test "$confirm" = 'y'
             /usr/local/bin/brew $argv
@@ -20,7 +18,7 @@ function brew
             echo "Okay. 🤟🏼"
         end
     else
-        echo "Quitting. Either \$HOMEBREW_NO_INSTALL_CLEANUP ('$noinstall') isn't set in env, or \$HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS ('$noperiodic') isn't set high enough."
+        echo -e "Quitting. Either \$HOMEBREW_NO_INSTALL_CLEANUP ('$noinstall') isn't set in env, or\n\$HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS ('$noperiodic') isn't set (or isn't large enough)."
         return 1
     end
 end
