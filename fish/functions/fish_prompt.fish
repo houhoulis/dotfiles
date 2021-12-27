bad05edaf94d1a677ada19064266be77259efc13
Chris Houhoulis <chris@chrishouhoulis.com>
2021-12-27 12:59:53 -0500

    Add current git branch (if exists) to fish prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,14 @@
 function fish_prompt
-    # dunno the canonical way to display abbreviated home path ("~") when you can
-
-#    set_color purple
-
-    echo "$status" (set_color purple)(echo (pwd) | sed s=$HOME=~=) "⚡"(set_color 491)"·› "(set_color normal)
     # set -l -u current_path (echo (pwd) | sed s=$HOME=~=)
     # echo $current_path "⚡"(set_color 491)"·› "(set_color normal)
+
+    set -l gitbranch (git branch --show-current 2>/dev/null)
+    if test -n "$gitbranch"
+        set gitbranch (set_color 491)"($gitbranch) "
+    else
+        set gitbranch ''
+    end
+    # dunno the canonical way to substitute abbreviated home path ("~") when appropriate
+    echo "$status" (set_color brblue)(echo (pwd) | sed s=$HOME=~=)" "(echo "$gitbranch")"⚡"(set_color brmagenta)"·› "(set_color normal)
 end
 
