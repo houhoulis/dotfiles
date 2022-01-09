e14154acc3b4a58bc4e4a2b86d0ffb73fe236773
Chris Houhoulis <chris@chrishouhoulis.com>
2022-01-09 17:05:44 -0500

    Add "dirty" marker to git branch in prompt.
    Uses the output of `git status --short`, which seems to include a code
    of one letter for each file in the index or "??" for an untracked
    file. This commit concatenates those letters and "??"s as the marker
    in the prompt for a "dirty" git status. The marker shows next to the
    git branch name when in a git project directory.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,10 +4,17 @@ function fish_prompt
 
     set -l gitbranch (git branch --show-current 2>/dev/null)
     if test -n "$gitbranch"
-        set gitbranch (set_color 491)"($gitbranch) "
-    else
-        set gitbranch ''
+        if test -n (echo (git status --short))
+            set -l dirty ''
+            string split "\n" (git status --short) | while read -l dirtyline
+                set dirty $dirty(string split -n -f1 ' ' $dirtyline)
+            end
+            set gitbranch (set_color 491)"($gitbranch $dirty"(set_color 491)") "
+        else
+            set gitbranch (set_color 491)"($gitbranch) "
+        end
     end
+
     # dunno the canonical way to substitute abbreviated home path ("~") when appropriate
     echo "$status" (set_color brblue)(echo (pwd) | sed s=$HOME=~=)" "(echo "$gitbranch")"⚡"(set_color brmagenta)"·› "(set_color normal)
 end
