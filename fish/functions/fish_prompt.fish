9dd1d4a2f71042858ff82440617c25c3f235e016
Chris Houhoulis <chris@chrishouhoulis.com>
2022-02-18 15:16:25 -0500

    fish_prompt: Display previous command's exit code.
    Grab the exit code ($status) at the beginning of the fish_prompt
    function, before the function overwrites the status via its own
    internal commands. This broke from fish 3.1 to 3.3.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,5 @@
 function fish_prompt
-    # set -l -u current_path (echo (pwd) | sed s=$HOME=~=)
-    # echo $current_path "⚡"(set_color 491)"·› "(set_color normal)
+    set -l previous_status $status
 
     set -l gitbranch (git branch --show-current 2>/dev/null)
     if test -n "$gitbranch"
@@ -16,6 +15,6 @@ function fish_prompt
     end
 
     # dunno the canonical way to substitute abbreviated home path ("~") when appropriate
-    echo "$status" (set_color brblue)(echo (pwd) | sed s=$HOME=~=)" "(echo "$gitbranch")"⚡"(set_color brmagenta)"·› "(set_color normal)
+    echo "$previous_status" (set_color brblue)(echo (pwd) | sed s=$HOME=~=)" "(echo "$gitbranch")"⚡"(set_color brmagenta)"·› "(set_color normal)
 end
 
