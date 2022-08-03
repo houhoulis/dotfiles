80c5d1b7a9a8e15849d9ae3d2f43e226e4ab7ca7
Chris Houhoulis <chris@chrishouhoulis.com>
2022-08-02 23:56:54 -0400

    Make `where` use sed instead of sd for portability

--- fish/functions/where.fish
+++ fish/functions/where.fish
@@ -1,4 +1,5 @@
 # https://github.com/masukomi/masuconfigs/blob/master/.config/fish/functions/where.fish
+# edited to use `sed` instead of `sd`
 
 function where --argument-names 'exe_or_function' \
   --description "finds an executable or fish function with the supplied name"
@@ -8,7 +9,7 @@ function where --argument-names 'exe_or_function' \
     echo $exe_path
     return 0
   else
-    set function_names (functions --names | sd ", " "\n" | grep "^$exe_or_function")
+    set function_names (functions --names | sed s/,\ /\\n/g | grep "^$exe_or_function")
     for x in $function_names
       set function_path (functions -D $x)
       echo $function_path
