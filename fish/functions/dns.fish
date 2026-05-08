4387cafca7a47c6a91b51e943aff190972dbba33
Chris Houhoulis <chris@chrishouhoulis.com>
2026-05-07 21:25:59 -0400

    More accurate description of `dns` function.

--- fish/functions/dns.fish
+++ fish/functions/dns.fish
@@ -1,4 +1,4 @@
-function dns --argument-names 'fqdn' --description "Look up IPs via the Mac OS DNS utility. Better than dig."
+function dns --argument-names 'fqdn' --description "Look up IPs via the Mac OS DNS utility. Alternative to dig or nslookup."
     dscacheutil -q host -a name $fqdn
 end
 
