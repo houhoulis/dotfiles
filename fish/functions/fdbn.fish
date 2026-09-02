774c8d9026f79ad95c6ffe486a18bb30521e4ae9
Chris Houhoulis <chris@chrishouhoulis.com>
2026-09-01 20:06:25 -0400

    Remove "fdbn" function; it adds no value to fd.

--- fish/functions/fdbn.fish
+++
@@ -1,9 +0,0 @@
-# based on 'fbn' from masukomi, but ported from find to fd
-function fdbn --argument-names 'filename' 'path' -d "Look for a file by filename pattern under the passed-in dir (2nd arg) or else current dir"
-    if test "$path" = ""
-        fd "$filename"
-    else
-        fd "$filename" "$path"
-    end
-end
-
