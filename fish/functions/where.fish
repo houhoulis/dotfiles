d33af328f35add3acd3a45c183025afd0c79bbe7
Chris Houhoulis <chris@chrishouhoulis.com>
2026-09-01 20:34:14 -0400

    "where" returns both OS binaries and functions.
    Instead of exiting when a binary is found, continue on to check for
    fish functions as well. Return all that are found.
    
    Slight fix to the grep argument.
    
    This still doesn't work for some executables such as "set" and "open".
    "set" is not found by this function at all; it's not in
    `functions --names`. "open" is included in `functions --names`, but
    this function returns "n/a" for the fish-function portion of the output
    (as does `functions -D open`).

--- fish/functions/where.fish
+++ fish/functions/where.fish
@@ -1,21 +1,24 @@
 # https://github.com/masukomi/masuconfigs/blob/master/.config/fish/functions/where.fish
 # edited to use `sed` instead of `sd`
+# edited to return both binaries & fish functions if both found
 
 function where --argument-names 'exe_or_function' \
-  --description "finds an executable or fish function with the supplied name"
+  --description "finds executable and fish function(s) with the supplied name"
 
-  set exe_path (command -v $exe_or_function)
+  set -l where_status 1
+
+  set -l exe_path (command -v $exe_or_function)
   if test $status -eq 0
     echo $exe_path
-    return 0
-  else
-    set function_names (functions --names | sed s/,\ /\\n/g | grep "^$exe_or_function")
-    for x in $function_names
-      set function_path (functions -D $x)
-      echo $function_path
-    end
-    return 0
+    set where_status 0
   end
-  return 1
+
+  set -l function_names (functions --names | sed s/,\ /\\n/g | grep "^$exe_or_function\$")
+  for fun in $function_names
+    echo (functions -D $fun)
+    set where_status 0
+  end
+
+  return $where_status
 end
 
